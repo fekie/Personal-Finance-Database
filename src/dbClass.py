ee4a93cf884927625eb44aff6e72f7125f078a3c
ChloeChanUwU <fexykthefox@gmail.com>
2021-06-02 16:09:10 -0400

    added db object

--- src/dbClass.py
+++ src/dbClass.py
@@ -1,4 +1,21 @@
+import os
+import sqlite3
+import time
+import math
+from datetime import datetime
+
 class db:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    
+    def __init__(self, directory, version, assetTypeList):
+        if not os.path.exists(directory + "/db_v" + version):
+            os.makedirs(directory + "/db_v" + version)
+                
+        self.Connection = sqlite3.connect(directory + "/db_v" + version + "/db_v" + version + ".db")
+        self.Cursor = self.Connection.cursor()
+        
+        
+        self.Cursor.execute("CREATE TABLE IF NOT EXISTS balance (asset_type TEXT PRIMARY KEY, amount FLOAT, last_time_string TEXT, last_time_unix INTEGER)")
+        
+        for type in assetTypeList:
+            self.Cursor.execute("INSERT OR IGNORE INTO balance (asset_type, amount, last_time_string, last_time_unix) VALUES (?, ?, ?, ?)", (type, 0.0, datetime.today().strftime('%m-%d-%Y||%H:%M:%S'), int(time.time())))
+        self.Connection.commit()
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import sqlite3
 import json
+import dbClass as dbClass
 
 global config
 with open("config.json") as f:
   config = json.load(f)
 
-con = sqlite3.connect("../databases/db_v" + config["DbVersion"] + "/db_v" + config["DbVersion"] + ".db")
+db = dbClass.db(config["DatabasesPath"], config["DbVersion"], config["AssetTypes"])
+
+''' con = sqlite3.connect("../databases/db_v" + config["DbVersion"] + "/db_v" + config["DbVersion"] + ".db")
 cur = con.cursor()
 
 #cur.execute()
 con.commit()
 
-con.close()
+con.close() '''
